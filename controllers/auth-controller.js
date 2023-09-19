a9bbfb0cd397ff3bd49bde9d54b99d64e51a2f43
Senriv <ikiry2013@gmail.com>
2023-09-19 17:53:00 +0300

    fix hw

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -41,13 +41,32 @@ const signin = async (req, res) => {
   };
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(id, { token });
 
   res.json({
     token,
   });
 };
 
+const getCurrent = async (req, res) => {
+  const { email } = req.user;
+  res.json({
+    email,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "No Content",
+  });
+};
+
 export default {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -3,6 +3,7 @@ import express from "express";
 import authController from "../../controllers/auth-controller.js";
 import validateBody from "../../decorators/validateBody.js";
 import { userSignupOrSigninSchema } from "../../models/User.js";
+import { authenticate } from "../../middlewares/index.js";
 
 const authRouter = express.Router();
 
@@ -17,5 +18,9 @@ authRouter.post(
   validateBody(userSignupOrSigninSchema),
   authController.signin
 );
+
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.post("/logout", authenticate, authController.logout);
 
 export default authRouter;
