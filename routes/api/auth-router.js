6a5709004f0e4ececa7c51c1e44a2609f05cb1e0
Senriv <ikiry2013@gmail.com>
2023-09-27 23:19:42 +0300

    finish hw

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,7 +2,10 @@ import express from "express";
 
 import authController from "../../controllers/auth-controller.js";
 import validateBody from "../../decorators/validateBody.js";
-import { userSignupOrSigninSchema } from "../../models/User.js";
+import {
+  userSignupOrSigninSchema,
+  userEmailSchema,
+} from "../../models/User.js";
 import { authenticate, upload } from "../../middlewares/index.js";
 
 const authRouter = express.Router();
@@ -23,6 +26,19 @@ authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.logout);
 
-authRouter.patch("/avatars", authenticate, upload.single("avatar"), authController.updateAvatar);
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
+
+authRouter.get("/verify/:verificationToken", authController.verify);
+
+authRouter.post(
+  "/verify",
+  validateBody(userEmailSchema),
+  authController.resendVerifyEmail
+);
 
 export default authRouter;
